d07b13098e4aac25e2798a7f54939c037e358765
Joao.Miranda <joao_to10@hotmail.com>
2023-09-03 14:08:24 -0300

    #First commit

---
+++ src/main/java/com/example/Authentication/DTO/PessoaDTO.java
@@ -0,0 +1,45 @@
+package com.example.Authentication.DTO;
+
+import com.example.Authentication.model.FileName;
+import com.example.Authentication.model.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class PessoaDTO {
+    private String nome;
+
+    private String sobrenome;
+
+    private Date data_nascimento;
+
+    private String cpf;
+
+    private String rg;
+
+    private String endereco;
+
+    private String cidade;
+
+    private String estado;
+
+    private String cep;
+
+    private String telefone;
+
+    private String email;
+
+    private Usuario usuario;
+
+    private FileName file;
+
+    private Short role;
+
+}
